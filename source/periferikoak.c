3a42d04cc8a6a223cf842b60efdec1abb5f45616
Julen <Julortizw@gmail.com>
2019-04-08 09:53:29 +0200

    Periferikoak bete

--- source/periferikoak.c
+++ source/periferikoak.c
@@ -24,21 +24,30 @@ int SakatutakoTekla()
 	//Gora=6;Behera=7;R=8;L=9;
 
 
-	
+	if ((TEKLAK_DAT & 0x0001) == 0) return A;
+	if ((TEKLAK_DAT & 0x0002) == 0) return B;
+	if ((TEKLAK_DAT & 0x0004) == 0) return SELECT;
+	if ((TEKLAK_DAT & 0x0008) == 0) return START;
+	if ((TEKLAK_DAT & 0x0010) == 0) return ESKUBI;
+	if ((TEKLAK_DAT & 0x0020) == 0) return EZKER;
+	if ((TEKLAK_DAT & 0x0040) == 0) return GORA;
+	if ((TEKLAK_DAT & 0x0080) == 0) return BEHERA;
+	if ((TEKLAK_DAT & 0x0100) == 0) return R;
+	if ((TEKLAK_DAT & 0x0200) == 0) return L;
 
 }
 
 void konfiguratuTeklatua(int TEK_konf)
 {
 	//Teklatuaren konfigurazioa bere S/I erregistroak aldatuz
-	
+	TEKLAK_KNT = TEKLAK_KNT | TEK_konf;
 }
 
 void konfiguratuTenporizadorea(int Latch, int TENP_konf)
 {
 	//Tenporizadorearen konfigurazioa bere S/I erregistroak aldatuz
 	DENB0_DAT = Latch;
-	DENB0_KNT = TENP_konf;
+	DENB0_KNT = DENB0_KNT | TENP_konf;
 	
 	
 }
@@ -49,7 +58,7 @@ void TekEtenBaimendu()
 	//Lan hau burutzeko lehenengo eten guztiak galarazi behar dira eta bukaeran baimendu 
 	IME=0;
 	// HEMEN IDATZI BEHAR DUZUE ZUEN KODEA
-	
+	IE = IE | 0x00001000;
 	IME=1;
 }
 
@@ -60,7 +69,7 @@ void TekEtenGalarazi()
 	//Lan hau burutzeko lehenengo eten guztiak galarazi behar dira eta bukaeran baimendu 
 	IME=0;
 	// HEMEN IDATZI BEHAR DUZUE ZUEN KODEA
-	
+	IE = IE | 0xFFFFEFFF;
 	IME=1;
 }  
 
@@ -71,7 +80,7 @@ void DenbEtenBaimendu()
 //Horretarako lehenengo eten guztiak galarazi eta bukaeran berriro baimendu
 	IME=0;
 	// HEMEN IDATZI BEHAR DUZUE ZUEN KODEA
-	
+	IE = IE | 0x00000008;
 	IME=1;
 }
 
@@ -82,7 +91,7 @@ void DenbEtenGalarazi()
 //Horretarako lehenengo eten guztiak galarazi eta bukaeran berriro baimendu
 	IME=0;
 	// HEMEN IDATZI BEHAR DUZUE ZUEN KODEA
-	
+	IE = IE & 0xFFFFFFF7;
 	IME=1;
 
 }
@@ -90,11 +99,11 @@ void DenbEtenGalarazi()
 void ErlojuaMartxanJarri()
 {
 	// HEMEN IDATZI BEHAR DUZUE ZUEN KODEA
-	
+	DENB0_KNT = DENB0_KNT | 0x0080;
 }
 
 void ErlojuaGelditu()
 {
 	// HEMEN IDATZI BEHAR DUZUE ZUEN KODEA
-	
+	DENB0_KNT = DENB0_KNT & 0xFF7F;
 }
